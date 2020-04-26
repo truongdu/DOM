458644b60ed44855c41d3f2512bdc49de9f71552
TruongDu <truongdu.td88@gmailcom>
2020-04-26 20:29:25 +0700

    removeClass

--- main.js
+++ main.js
@@ -34,7 +34,33 @@ function addClass(el, className){
 }
 
 function removeClass(el, className){
-
+	/*
+		Nếu oldClass có tồn tại => tiến hành kiểm tra logic removeClass
+			nếu className tồn tại trong oldClass => tiếp tục xử lỹ logic removeClass
+				đưa oldClass về 1 arr => dùng split(' ');
+				kiếm tra xem className trong arr oldClass nằm tại vị trí thứ mấy => dùng arr.indexof(className)
+					nếu kết quả trả về -1 => không tìm thấy = > thì thôi.
+					kết kết quả trả về >=0 => dùng splice(indexof(className),1) để xóa 1 phần tử tại ví trí xuất hiện của className
+						trả về mảng đã được xóa.
+			nếu className không tồn tại trong oldClass => thì thôi
+		nếu oldClass không tồn tại => thì thôi.
+	*/
+	var oldClass = el.getAttribute("class");
+	if(oldClass){
+		var stringToArrClass = oldClass.split(" ");
+		console.log(stringToArrClass);
+		var checkClass = stringToArrClass.indexOf(className);
+		if(checkClass < 0){
+			console.log(oldClass);
+		}else{
+			stringToArrClass.splice(checkClass,1);
+			var arrToStringClass = stringToArrClass.join(" ");
+			console.log(arrToStringClass);
+		}
+	}else{
+		console.log("Class khong ton tai");
+	}
 }
 // addClass(changeContainerClass,'abc');
 // addClass(h1,'conga');
+removeClass(changeContainerClass,'abc2');
